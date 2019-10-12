7700ab5ef2c1854756f663bd91f5be158bc06d98
kbberker <kbbuk90@gmail.com>
2019-10-12 16:16:52 +0100

    Turn ids into numbers

--- src/components/WheelView.jsx
+++ src/components/WheelView.jsx
@@ -18,37 +18,37 @@ const WheelView = () => {
   console.log(questions)
   const wheelItems = [
     {
-      id: '1',
+      id: 1,
       icon: <FontAwesomeIcon icon={faThumbsUp} size={"3x"} />,
       question: questions.goodnessQuestion.question.narrative
     },
     {
-      id: '2',
+      id: 2,
       icon: <FontAwesomeIcon icon={faHandshake} size={"3x"} />,
       question: questions.trustQuestion.question.narrative
     },
     {
-      id: '3',
+      id: 3,
       icon: <FontAwesomeIcon icon={faHandHoldingHeart} size={"3x"} />,
       question: questions.generosityQuestion.question.narrative
     },
     {
-      id: '4',
+      id: 4,
       icon: <FontAwesomeIcon icon={faHandRock} size={"3x"} />,
       question: questions.freedomQuestion.question.narrative
     },
     {
-      id: '5',
+      id: 5,
       icon: <FontAwesomeIcon icon={faPuzzlePiece} size={"3x"} />,
       question: 'Where do you find your identity?'
     },
     {
-      id: '6',
+      id: 6,
       icon: <FontAwesomeIcon icon={faLeaf} size={"3x"} />,
       question: 'What makes you feel alive?'
     },
     {
-      id: '7',
+      id: 7,
       icon: <FontAwesomeIcon icon={faLaughBeam} size={"3x"} />,
       question: 'How do you know if you are happy?'
     },
