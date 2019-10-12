7a7003d4461e2b7997551296d3f52e6064954a0d
Ella <evross97@gmail.com>
2019-10-12 15:06:09 +0100

    Imported icons

--- src/components/WheelView.jsx
+++ src/components/WheelView.jsx
@@ -1,44 +1,52 @@
 import React from "react";
 import Wheel from "./Wheel";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import {
+    faHandHoldingHeart,
+    faHandRock,
+    faHands,
+    faHandshake,
+    faLaughBeam,
+    faPuzzlePiece,
+    faThumbsUp
+} from '@fortawesome/free-solid-svg-icons'
 
 
 const WheelView = () => {
     const wheelItems = [
         {
             id:'1',
-            icon: <FontAwesomeIcon icon={faThumbsUp} />,
+            icon: <FontAwesomeIcon icon={faThumbsUp} size={"3x"}/>,
             question: 'Is there such a thing as perfect love?'
         },
         {
             id:'2',
-            icon: <FontAwesomeIcon icon={faHandshake}/>,
+            icon: <FontAwesomeIcon icon={faHandshake} size={"3x"}/>,
             question: 'What gift of generosity has impacted you the most?'
         },
         {
             id:'3',
-            icon: <FontAwesomeIcon icon={faHandHoldingHeart}/>,
+            icon: <FontAwesomeIcon icon={faHandHoldingHeart} size={"3x"}/>,
             question: 'What gift of generosity has impacted you the most?'
         },
         {
             id:'4',
-            icon: <FontAwesomeIcon icon={faHandRock}/>,
+            icon: <FontAwesomeIcon icon={faHandRock} size={"3x"}/>,
             question: 'How can we become free?'
         },
         {
             id:'5',
-            icon: <FontAwesomeIcon icon={faPuzzlePiece}/>,
+            icon: <FontAwesomeIcon icon={faPuzzlePiece} size={"3x"}/>,
             question: 'Where do you find your identity?'
         },
         {
             id:'6',
-            icon: <FontAwesomeIcon icon={faHands}/>,
+            icon: <FontAwesomeIcon icon={faHands} size={"3x"}/>,
             question: 'What makes you feel alive?'
         },
         {
             id:'7',
-            icon: <FontAwesomeIcon icon={faLaughBeam}/>,
+            icon: <FontAwesomeIcon icon={faLaughBeam} size={"3x"}/>,
             question: 'How do you know if you are happy?'
         },
     ]
